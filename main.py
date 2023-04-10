e48201f77a9fd8d722f5115fdc7baa9c855c76a7
schen24 <schen638@yahoo.com>
2023-04-10 15:36:32 +0800

    fixed utf

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 import requests
 from sendemail import send_email
+from datetime import date, timedelta
+
+stock='chatgpt'
+fromdate = date.today()
+fromdate = str(fromdate- timedelta(days=1))
 
 
-url = "https://newsapi.org/v2/everything?q=tesla&" \
-    "from=2023-03-08&sortBy=publishedAt&"\
-    "apiKey=297dd2fa073043748f2f8616c02d8204"
+url = f'https://newsapi.org/v2/everything?q={stock}' \
+    f'&from={fromdate}' \
+      '&sortBy=publishedAt'\
+    '&apiKey=297dd2fa073043748f2f8616c02d8204' \
+      '&language=en'
 
-#apikey = "297dd2fa073043748f2f8616c02d8204"
+print(url)
 
 requ = requests.get(url)
 #json gives dict, text gives string
 contents = requ.json()
-
+print(contents)
 
 message_body = ""
-for article in contents["articles"]:
-    message_body = f'{message_body} \n\n {article["title"]} ' \
-                   f'\n {article["description"]}'
+for article in contents["articles"][:50]:
+    if article['title'] is not None:
+        message_body = f'{message_body} \n\n {article["title"]} ' \
+                   f'\n {article["description"]}' \
+                   f'\n {article["url"]}' 
 
-message_body = message_body.encode('utf-8')
+
 print(message_body)
 message = f"""\
 subject:news from apifeed 
 {message_body}"""
 
-send_email(message_body)
+#code entire message in utf for it to work, not just message body
+message = message.encode('utf-8')
+
+send_email(message)
